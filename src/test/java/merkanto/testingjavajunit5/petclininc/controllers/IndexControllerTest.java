a2f37c64a6c6553a2215000e179b797b1219e7d2
Merkanto <miroslav.i.zhelezchev@gmail.com>
2023-01-04 15:21:45 +0200

    Adding JUnit Test Display Names.

--- src/test/java/merkanto/testingjavajunit5/petclininc/controllers/IndexControllerTest.java
+++ src/test/java/merkanto/testingjavajunit5/petclininc/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package merkanto.testingjavajunit5.petclininc.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,6 +17,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test Proper View name is returned for index page")
     void index() {
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "Wrong View Returned");
@@ -23,6 +25,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test exception")
     void oupsHandler() {
         assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive message to build for my test");
     }

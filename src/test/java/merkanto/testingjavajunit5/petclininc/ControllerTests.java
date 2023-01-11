b271c4b636179a19fab655ee945413c33f131f25
Merkanto <miroslav.i.zhelezchev@gmail.com>
2023-01-11 17:49:02 +0200

    Adding JUnit Default Test Methods.

--- src/test/java/merkanto/testingjavajunit5/petclininc/ControllerTests.java
+++ src/test/java/merkanto/testingjavajunit5/petclininc/ControllerTests.java
@@ -1,7 +1,15 @@
 package merkanto.testingjavajunit5.petclininc;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controllers")
 public interface ControllerTests {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Let's do something here");
+    }
 }

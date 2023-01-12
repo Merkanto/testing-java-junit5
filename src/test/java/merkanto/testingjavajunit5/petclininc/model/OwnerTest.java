37ffa4d8d27994319266ab5a48c292846a7719c0
Merkanto <miroslav.i.zhelezchev@gmail.com>
2023-01-12 14:10:32 +0200

    Adding JUnit Parameterized Tests - Value Source.

--- src/test/java/merkanto/testingjavajunit5/petclininc/model/OwnerTest.java
+++ src/test/java/merkanto/testingjavajunit5/petclininc/model/OwnerTest.java
@@ -2,6 +2,8 @@ package merkanto.testingjavajunit5.petclininc.model;
 
 import merkanto.testingjavajunit5.petclininc.ModelTests;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -25,5 +27,11 @@ class OwnerTest implements ModelTests {
                 ));
 
         assertThat(owner.getCity(), is("Key West"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Spring", "Framework", "Guru"})
+    void testValuSource(String val) {
+        System.out.println(val);
     }
 }
